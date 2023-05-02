9c8334aa381fadec9c7f7b715ac1198fc0d07a86
Koen Dirk van Esterik <info@koendirkvanesterik.com>
2023-05-02 12:55:35 +0200

    fix: adjust user story generating script

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -1,9 +1,10 @@
-ticket() {
+story() {
     [ -n "$1" ] && sh "$HOME/.dotfiles/scripts/openai.sh" \
-        "I want you to write a Jira ticket in the following format:
-            description, functional notes, acceptance criteria, test notes. Add
-            to test notes to functionally test with unit tests and visually
-            test with Storybook stories. Answer in markdown with aforementioned
-            topics in bold and possible component names wrapped in backticks.
+        "I want you to write a user story in the following format:
+            description, functional notes, acceptance criteria, test notes.
+            Add to test notes to visually test in Storybook.
+            Answer in markdown with the description, functional notes,
+            acceptance criteria and test notes headers in bold and possible
+            component names wrapped in backticks.
             The input is: $1"
 }
